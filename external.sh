df91255e31db64aefdf181f1d87af83b28937895
Andy Yang <andytheyang@gmail.com>
2017-04-04 08:46:39 -0700

    install.sh

--- external.sh
+++ external.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 gsettings set org.gnome.desktop.interface text-scaling-factor 1
-# gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "[{'Gdk/WindowScalingFactor', <1>}]"
 
 exit 0;
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# installs i3 configurations
+ln -sr i3config ~/.config/i3/config
+ln -sr wallpaper.jpg ~/.config/i3/wallpaper.jpg
+ln -sr i3blocks ~/.config/i3blocks/config
+ln -sr i3blocksscripts ~/.i3blocks
+ln -sr Xresources ~/.Xresources
