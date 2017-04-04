9d774c98aa995e3d15514f4e534ff53fd4ade632
Andy Yang <andytheyang@gmail.com>
2017-04-04 09:48:16 -0700

    updated laptop and external for swapping values in configs

--- external.sh
+++ external.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# set gnome text scaling
 gsettings set org.gnome.desktop.interface text-scaling-factor 1
+
+sed -i -e 's/height 80		# REPLACE/height 40		# REPLACE/g' i3config
+sed -i -e 's/exec --no-startup-id xrandr --dpi 240/# exec --no-startup-id xrandr --dpi 240/g' i3config
+
+sed -i -e 's/Xft.dpi: 240/!Xft.dpi: 240/g' Xresources
+
+sed -i -e 's/separator_block_width=50/separator_block_width=25/g' i3blocks
 
 exit 0;
--- laptop.sh
+++ laptop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# set gnome text scaling
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+
+sed -i -e 's/height 40		# REPLACE/height 80		# REPLACE/g' i3config
+sed -i -e 's/# exec --no-startup-id xrandr --dpi 240/exec --no-startup-id xrandr --dpi 240/g' i3config
+
+sed -i -e 's/!Xft.dpi: 240/Xft.dpi: 240/g' Xresources
+
+sed -i -e 's/separator_block_width=25/separator_block_width=50/g' i3blocks
 
 exit 0;
