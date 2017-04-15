3f84b694dd6c024078a6b773f04d19f8feebe78c
Andy Yang <andytheyang@gmail.com>
2017-04-14 21:01:24 -0700

    updated wallpaper and change dpi mode scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # installs i3 configurations
-ln -sr i3config ~/.config/i3/config
-ln -sr wallpaper.jpg ~/.config/i3/wallpaper.jpg
-ln -sr i3blocks ~/.config/i3blocks/config
-ln -sr i3blocksscripts ~/.i3blocks
-ln -sr Xresources ~/.Xresources
+ln -sr i3config ~/.config/i3/config					# i3 config
+ln -sr wallpaper.jpg ~/.config/i3/wallpaper.jpg		# wallpaper
+ln -sr i3blocks ~/.config/i3blocks/config			# i3blocks config
+ln -sr i3blocksscripts ~/.i3blocks					# i3blocks block scripts
+ln -sr Xresources ~/.Xresources						# Xresources for dpi
